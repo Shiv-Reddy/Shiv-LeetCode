e0390d62e7c83c0b1438f38d9e009d4af6a3107a
Shiv Reddy <159753751+Shiv-Reddy@users.noreply.github.com>
2026-08-24 18:58:17 +0530

    Time: 46 ms (47.99%), Space: 7.8 MB (81.12%) - LeetHub

---
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int count = 0;
+    
+    int countArrangement(int n) {
+        vector<bool> used(n + 1, false);
+        backtrack(n, 1, used);
+        return count;
+    }
+    
+    void backtrack(int n, int pos, vector<bool>& used) {
+        if (pos > n) {
+            count++;
+            return;
+        }
+        
+        for (int num = 1; num <= n; num++) {
+            if (!used[num] && (num % pos == 0 || pos % num == 0)) {
+                used[num] = true;
+                backtrack(n, pos + 1, used);
+                used[num] = false;
+            }
+        }
+    }
+};
