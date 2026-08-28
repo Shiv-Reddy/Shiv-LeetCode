72b287ff1f8ba2e2f2a5b8cb8855e8e5d17de9ff
Shiv Reddy <159753751+Shiv-Reddy@users.noreply.github.com>
2026-08-28 17:01:46 +0530

    Time: 7 ms (50.77%), Space: 13.4 MB (44.62%) - LeetHub

--- 3734-lexicographically-smallest-palindromic-permutation-greater-than-target/3734-lexicographically-smallest-palindromic-permutation-greater-than-target.cpp
+++ 3734-lexicographically-smallest-palindromic-permutation-greater-than-target/3734-lexicographically-smallest-palindromic-permutation-greater-than-target.cpp
@@ -2,82 +2,59 @@ class Solution {
 public:
     string lexPalindromicPermutation(string s, string target) {
         int n = s.size();
-        vector<int> count(26, 0);
-        for (char c : s) count[c - 'a']++;
-
-        int oddCount = 0, oddChar = -1;
+        vector<int> cnt(26, 0);
+        for (char c : s) cnt[c - 'a']++;
+        
+        int oddCnt = 0, oddChar = -1;
         for (int c = 0; c < 26; c++) {
-            if (count[c] % 2 == 1) { oddCount++; oddChar = c; }
+            if (cnt[c] % 2) { oddCnt++; oddChar = c; }
         }
-        if (n % 2 == 0 && oddCount != 0) return "";
-        if (n % 2 == 1 && oddCount != 1) return "";
+        bool needMid = (n % 2 == 1);
+        if (needMid && oddCnt != 1) return "";
+        if (!needMid && oddCnt != 0) return "";
         
-        int half_len = n / 2;
-        int mid = n % 2;
-        char mid_char = (mid == 1) ? ('a' + oddChar) : 0;
-        
+        int half = n / 2;
+        char midChar = needMid ? ('a' + oddChar) : 0;
         vector<int> pairs(26);
-        for (int c = 0; c < 26; c++) pairs[c] = count[c] / 2;
-        
+        for (int c = 0; c < 26; c++) pairs[c] = cnt[c] / 2;
+
         vector<int> avail = pairs;
-        int matched_len = 0;
-        int bestDevPos = -1;
-        char bestDevChar = 0;
+        int matchedLen = 0, devPos = -1;
+        char devChar = 0;
         
-        for (int i = 0; i < half_len; i++) {
+        for (int i = 0; i < half; i++) {
             int t = target[i] - 'a';
             for (int c = t + 1; c < 26; c++) {
-                if (avail[c] > 0) {
-                    bestDevPos = i;
-                    bestDevChar = 'a' + c;
-                    break;
-                }
+                if (avail[c] > 0) { devPos = i; devChar = 'a' + c; break; }
             }
-            if (avail[t] > 0) {
-                avail[t]--;
-                matched_len++;
-            } else {
-                break;
-            }
+            if (avail[t] > 0) { avail[t]--; matchedLen++; }
+            else break;
         }
         
-        if (matched_len == half_len) {
-            string T1 = target.substr(0, half_len);
-            string rev = T1;
-            reverse(rev.begin(), rev.end());
+        if (matchedLen == half) {
+            string firstHalf = target.substr(0, half);
+            string mirrored(firstHalf.rbegin(), firstHalf.rend());
             
-            if (mid == 1) {
-                char tm = target[half_len];
-                if (mid_char > tm) {
-                    return T1 + mid_char + rev;
-                } else if (mid_char == tm) {
-                    string T2 = target.substr(half_len + 1);
-                    if (rev > T2) return T1 + mid_char + rev;
-                }
-            } else {
-                string T2 = target.substr(half_len);
-                if (rev > T2) return T1 + rev;
+            if (needMid) {
+                char tm = target[half];
+                if (midChar > tm) return firstHalf + midChar + mirrored;
+                if (midChar == tm && mirrored > target.substr(half + 1))
+                    return firstHalf + midChar + mirrored;
+            } else if (mirrored > target.substr(half)) {
+                return firstHalf + mirrored;
             }
         }
-        
-        if (bestDevPos == -1) return "";
+
+        if (devPos == -1) return "";
         
         vector<int> rem = pairs;
-        for (int j = 0; j < bestDevPos; j++) rem[target[j] - 'a']--;
-        rem[bestDevChar - 'a']--;
+        for (int j = 0; j < devPos; j++) rem[target[j] - 'a']--;
+        rem[devChar - 'a']--;
         
-        string firstHalf = target.substr(0, bestDevPos);
-        firstHalf += bestDevChar;
-        for (int c = 0; c < 26; c++) {
-            firstHalf += string(rem[c], 'a' + c);
-        }
+        string firstHalf = target.substr(0, devPos) + devChar;
+        for (int c = 0; c < 26; c++) firstHalf += string(rem[c], 'a' + c);
         
-        string result = firstHalf;
-        if (mid == 1) result += mid_char;
-        string rev = firstHalf;
-        reverse(rev.begin(), rev.end());
-        result += rev;
-        
-        return result;
+        string mirrored(firstHalf.rbegin(), firstHalf.rend());
+        return needMid ? firstHalf + midChar + mirrored : firstHalf + mirrored;
     }
 };
