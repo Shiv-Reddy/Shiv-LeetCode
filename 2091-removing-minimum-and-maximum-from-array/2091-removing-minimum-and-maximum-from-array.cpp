25d954fd33d04f012512c774877f24194d80b8f6
Shiv Reddy <159753751+Shiv-Reddy@users.noreply.github.com>
2026-08-30 14:33:46 +0530

    Time: 0 ms (100%), Space: 92.2 MB (22.14%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
@@ -4,18 +4,24 @@ public:
         int n = nums.size();
         if (n == 1) return 1;
         
+        const int* p = nums.data();
+        int minVal = p[0], maxVal = p[0];
         int minIdx = 0, maxIdx = 0;
-        int minVal = nums[0], maxVal = nums[0];
         
         for (int i = 1; i < n; i++) {
-            int v = nums[i];
+            int v = p[i];
             if (v < minVal) { minVal = v; minIdx = i; }
             else if (v > maxVal) { maxVal = v; maxIdx = i; }
         }
         
-        int i = min(minIdx, maxIdx);
-        int j = max(minIdx, maxIdx);
+        int i = minIdx < maxIdx ? minIdx : maxIdx;
+        int j = minIdx < maxIdx ? maxIdx : minIdx;
         
-        return min({j + 1, n - i, (i + 1) + (n - j)});
+        int opt1 = j + 1;
+        int opt2 = n - i;
+        int opt3 = (i + 1) + (n - j);
+        
+        int ans = opt1 < opt2 ? opt1 : opt2;
+        return ans < opt3 ? ans : opt3;
     }
 };
